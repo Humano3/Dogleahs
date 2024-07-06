c11f4a9895922a49e810f405c23ebd5f20044d72
ValdoDlc <156165730+ValdoDLc@users.noreply.github.com>
2024-07-06 02:08:25 +0000

    Creacion de base de datos

---
+++ init.sql
@@ -0,0 +1,63 @@
+Create table admin(
+    id int,
+    administrador text,
+    password text
+);
+
+Create table cliente(
+    id int,
+    name varchar(10),
+    apellidos varchar(10),
+    email text,
+    password text
+);
+
+Create table numero(
+    id int,
+    Mascota varchar(15),Create table numero(
+    id int,
+    Mascota varchar(15),
+    Tipo varchar(10),
+    Raza varchar(10),
+    Numero varchar(10)
+);Create table cliente(
+    id int,
+    name varchar(10),
+    apellidos varchar(10),
+    email text,
+    password text,
+);Create table admin(
+    id int,
+    administrador text,
+    password text,
+);Create table admin(
+    id int,
+    administrador text,
+    password text,
+);
+
+Create table cliente(
+    id int,
+    name varchar(10),
+    apellidos varchar(10),
+    email text,
+    password text,
+);
+
+Create table numero(
+    id int,
+    Mascota varchar(15),
+    Tipo varchar(10),
+    Raza varchar(10),
+    Numero varchar(10)
+);Create table numero(
+    id int,
+    Mascota varchar(15),
+    Tipo varchar(10),
+    Raza varchar(10),
+    Numero varchar(10)
+);
+    Tipo varchar(10),
+    Raza varchar(10),
+    Numero varchar(10)
+);
