88af8e230433940813ea2ee607314f403ec7433d
ValdoDlc <156165730+ValdoDLc@users.noreply.github.com>
2024-07-18 03:51:30 +0000

    Api para users

---
+++ Api/db.php
@@ -0,0 +1,15 @@
+<?php
+
+function conexionBD(){
+    $dbHost='db';
+    $dbName='mariadb';
+    $dbUser='mariadb';
+    $dbPass='mariadb';
+    try{
+        $pdo=new PDO("mysql:host=$dbHost;dbname=$dbName",$dbUser,$dbPass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        }catch (Exception $e){
+        echo $e->getMessage();
+    }
+}
+?>
---
+++ Api/users.php
@@ -0,0 +1,19 @@
+<?php 
+header('Content-Type: application/json');
+
+include "../Modelo/Conexion.php";
+
+try {
+
+    $method = $_SERVER['REQUEST_METHOD'];
+
+    if($method == 'GET'){
+        $stmt = $pdo->query('SELECT id, name, apellidos, email FROM cliente');
+        $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($users); 
+    }
+} catch (Exception $e) {
+    http_response_code(500); 
+    echo json_encode(['error' => $e->getMessage()]); 
+}
+?>
